a8f18e9c4699e16f254808de48ab685f46fd7234
cperales <cperales@uloyola.es>
2018-03-27 19:27:59 +0200

    Temporary solution

--- tests/get_info_bioshock_infinite.py
+++ tests/get_info_bioshock_infinite.py
@@ -5,5 +5,8 @@ connection = APIConnection(api_key="A9AD261A3278990F62FD6A6193BEC570",
                            validate_key=True)
 response = connection.call(interface='ISteamUserStats', command='GetSchemaForGame', version='v2', appid='8870')
 
-with open('response_bioshock.json', 'w') as outfile:
-    json.dump(response, outfile)
+print(response)
+
+# with open('response_bioshock.json', 'w') as outfile:
+#     response_dict = response.__dict__
+#     json.dump(response, outfile)
